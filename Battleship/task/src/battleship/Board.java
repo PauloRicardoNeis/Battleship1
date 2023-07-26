94fa3cc03eb4652e834b8bba7dac2e7308b70279
Normie <paulonormie@gmail.com>
2023-07-26 03:00:47 -0300

    :3

--- Battleship/task/src/battleship/Board.java
+++ Battleship/task/src/battleship/Board.java
@@ -1,10 +1,11 @@
 package battleship;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Board {
     //[10][10] array, each position with 3 possible values
-    private Cell[][] cells;
+    public Cell[][] cells;
 
     Board() {
         cells = new Cell[10][10];
@@ -24,20 +25,129 @@ public class Board {
         return "Enter the coordinates of the " + boatText;
     }
 
-    private boolean placeBoat(String coordinates) {
-        char row1 = coordinates.charAt(0);
-        char column1 = coordinates.charAt(1);
-        char row2 = coordinates.charAt(3);
-        char column2 = coordinates.charAt(4);
+    private String placementState(int length, int row1, int row2, int column1, int column2) {
 
-        boolean placementIsValid = false;
-
-        if (row1 == row2 || column1 == column2) {
-            placementIsValid = true;
+        if (row1 == row2 && Math.abs(column1 - column2) == length - 1) {
+            if (checkPlacementAvailabilityHorizontal(length, row1, row2, column1, column2)) {
+                System.out.println("H");
+                return "horizontal";
+            }
         }
 
+        if (column1 == column2 && Math.abs(row1 - row2) == length - 1) {
+            if (checkPlacementAvailabilityVertical(length, row1, row2, column1, column2)) {
+                System.out.println("V");
+                return "vertical";
+            }
+        }
 
-        return placementIsValid;
+        System.out.println("I");
+        return "invalid";
+    }
+
+    private boolean checkPlacementAvailabilityVertical(int length, int row1, int row2, int column1, int column2) {
+        int min = row1 < row2 ? row1 : row2;
+        System.out.println(min);
+
+        for(int i = min; i < length + min; i++) {
+            if (checkCellAvailavility(column1, i))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkPlacementAvailabilityHorizontal(int length, int row1, int row2, int column1, int column2) {
+        int min = column1 < column2 ? column1 : column2;
+        System.out.println(min);
+
+        for(int i = min; i < length + min; i++) {
+            if (checkCellAvailavility(i, row1))
+                return false;
+        }
+
+        return true;
+    }
+
+    public void placeBoat(String coordinates, int length) {
+        Scanner coordStream = new Scanner(coordinates);
+
+        System.out.println(coordinates);
+
+        int[] coordinateArr = parsePlacementCode(coordinates);
+
+        int row1 = coordinateArr[0];
+        int column1 = coordinateArr[1];
+        int row2 = coordinateArr[2];
+        int column2 = coordinateArr[3];
+
+        System.out.println(column1 + " " + column2);
+        System.out.println(row1 + " " + row2);
+
+        String direction = placementState(length, row1, row2, column1, column2);
+        if (direction.equals("horizontal")) {
+            horizontalFill(row1, row2, column1, column2, length);
+        }
+        if (direction.equals("vertical")) {
+            verticalFill(row1, row2, column1, column2, length);
+        }
+        else {
+            System.out.println(direction);
+        }
+
+    }
+
+    public int[] parsePlacementCode(String code) {
+        int[] toReturn = new int[4];
+        String[] strings = code.split(" ");
+        System.out.println(strings[0] + " " + strings[1]);
+
+        toReturn[0] = strings[0].charAt(0) - 65;
+        toReturn[2] = strings[1].charAt(0) - 65;
+
+        toReturn[1] = Integer.parseInt((strings[0].split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)")[1])) - 1;
+        toReturn[3] = Integer.parseInt((strings[1].split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)")[1])) - 1;
+
+        System.out.println(Arrays.toString(toReturn));
+
+        return toReturn;
+    }
+
+    private void horizontalFill(int row1, int row2, int column1, int column2, int length) {
+        int min = column1 < column2 ? column1 : column2;
+        System.out.println(min);
+
+        for(int i = min; i < length + min; i++) {
+            cells[i][row1] = Cell.O;
+        }
+    }
+
+    private void verticalFill(int row1, int row2, int column1, int column2, int length) {
+        int min = row1 < row2 ? row1 : row2;
+        System.out.println(min);
+
+        for(int i = min; i < length + min; i++) {
+            cells[column1][i] = Cell.O;
+        }
+    }
+
+    public boolean checkCellAvailavility(int row, int column) {
+        System.out.println(row + " " + column);
+        if (cellIsOutOfBounds(row, column)) return false;
+        if (!cellIsOutOfBounds(row, column) && !cellIsFog(row, column)) return false;
+        if (!cellIsOutOfBounds(row, column + 1) && !cellIsFog(row, column + 1)) return false;
+        if (!cellIsOutOfBounds(row, column - 1) && !cellIsFog(row, column - 1)) return false;
+        if (!cellIsOutOfBounds(row + 1, column) && !cellIsFog(row + 1, column)) return false;
+        if (!cellIsOutOfBounds(row - 1, column) && !cellIsFog(row - 1, column)) return false;
+        return true;
+    }
+
+    public boolean cellIsOutOfBounds(int row, int column) {
+        return row > 9 || row < 0 || column > 9 || column < 0;
+    }
+
+    public boolean cellIsFog (int row, int column) {
+        return this.cells[column + 1][row].equals(Cell.FOG);
     }
 
     @Override
@@ -52,8 +162,5 @@ public class Board {
         }
         return sb.toString();
     }
-
-
-
 
 }
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -4,15 +4,47 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void testSomething(int i, int j, Board board) {
+        board.cells[i][j] = Cell.M;
+        System.out.println(board);
+    }
+
+    public static String testCheckCell(String c, Board board) {
+        return c + ": " + board.checkCellAvailavility(c.charAt(0) - 65, Integer.parseInt((c.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)")[1])) - 1);
+    }
+
     public static void main(String[] args) {
         // Write your code here
         Board board = new Board();
         Scanner scan = new Scanner(System.in);
 
         System.out.println(board);
+        System.out.println(testCheckCell("B2", board));
+        System.out.println(testCheckCell("A1", board));
+        board.cells[1][1] = Cell.X;
+        System.out.println(board);
+        System.out.println(testCheckCell("A2", board));
+        System.out.println(testCheckCell("B1", board));
+        System.out.println(testCheckCell("B3", board));
+        System.out.println(testCheckCell("C1", board));
+        System.out.println(testCheckCell("A1", board));
+        System.out.println(testCheckCell("A3", board));
+        System.out.println(testCheckCell("C1", board));
+        System.out.println(testCheckCell("C3", board));
 
-        System.out.println(board.askForCoordinatesText(5));
-        String coordinate = scan.nextLine();
+
+
+
+
+
+        for (int i = 5; i > 2; i--) {
+            System.out.println(board.askForCoordinatesText(i));
+            board.placeBoat(scan.nextLine(), i);
+            System.out.println(board);
+
+        }
+
+
     }
 
 }
